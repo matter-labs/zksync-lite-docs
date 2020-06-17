de089e58bb9594925dbfcb3f16540434e0422153
Alex Gluchowski <ag@matterlabs.dev>
2020-06-17 17:23:40 +0200

    space

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -177,7 +177,7 @@ export default {
 
   .features {
     border-top: 1px solid $borderColor;
-    padding: 1.2rem 0 0.5rem;
+    padding: 2.5rem 0 2rem;
     margin-top: 2.5rem;
     display: flex;
     flex-wrap: wrap;
