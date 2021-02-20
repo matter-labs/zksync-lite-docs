d739e938a6af5d5ce3eae2c23d106de4b11eb0a4
barakshani <35497694+barakshani@users.noreply.github.com>
2021-02-20 19:33:08 +0200

    revert: welcome to zksync

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -55,8 +55,14 @@ module.exports = {
             ],
             '/faq/': [
                 {
-                    title: 'Overview',  // required
+                    title: 'Welcome to zkSync',  // required
                     path: '/faq/', // optional, which should be a absolute path.
+                    collapsable: true,  // optional, defaults to true
+                    sidebarDepth: 0,     // optional, defaults to 1
+                },
+                {
+                    title: 'Overview',  // required
+                    path: '/faq/intro.html', // optional, which should be a absolute path.
                     collapsable: true,  // optional, defaults to true
                     sidebarDepth: 1,     // optional, defaults to 1
                 },
