13916e0f7e1d9b4e587be519a745520741a2dee0
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2021-05-27 15:16:39 +0300

    last attempt (for now_

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -253,6 +253,24 @@ module.exports = {
     }
   },
   head: [
+    // And this hack is for making anchors work
+    [
+    'script', {},
+      ` 
+      window.onload = function() {
+        requestAnimationFrame(function() {
+          if (location.hash) {
+            const element = document.getElementById(location.hash.slice(1))
+      
+            if (element) {
+              element.scrollIntoView()
+            }
+          }
+        })
+      }
+      
+      `
+    ],
     ['script', { src: '/__/firebase/7.13.2/firebase-app.js', defer: true }, ''],
     ['script', { src: '/__/firebase/7.13.2/firebase-analytics.js', defer: true }, ''],
     ['script', { src: '/__/firebase/init.js', defer: true }, ''],
@@ -279,24 +297,6 @@ module.exports = {
     }
    }, 1000)`
     ],
-    // And this hack is for making anchors work
-    [
-      'script', {},
-      ` 
-      window.onload = function() {
-        requestAnimationFrame(function() {
-          if (location.hash) {
-            const element = document.getElementById(location.hash.slice(1))
-      
-            if (element) {
-              element.scrollIntoView()
-            }
-          }
-        })
-      }
-      
-      `
-    ]
   ],
 
 };
