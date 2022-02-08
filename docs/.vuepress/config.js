363b8caf51e362bb1633df5f75d39a51fd16419c
Sergey Beresnev <s.beresnev@matterlabs.dev>
2022-02-08 13:09:40 +0200

    fix: icons added & config.js fixed

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,263 +1,280 @@
-module.exports = { title: "zkSync: secure, scalable crypto payments", // adding title gives us a header with search box
-  description: "zkSync is a fully trustless user-centric zkRollup protocol for scaling payments and smart contracts on Ethereum.",
-  repo: "matter-labs/zksync",
-  dest: "dist",
+module.exports = {
+  title: 'zkSync: secure, scalable crypto payments', // adding title gives us a header with search box
+  description: 'zkSync is a fully trustless user-centric zkRollup protocol for scaling payments and smart contracts on Ethereum.',
+  repo: 'matter-labs/zksync',
+  dest: 'dist',
   markdown: {
     toc: { includeLevel: [2, 3] },
   },
   extendMarkdown: (md) => {
     // Add support of footnotes (like [^1]) in markdown
-    md.use(require("markdown-it-footnote"));
+    md.use(require('markdown-it-footnote'));
   },
   plugins: {
-    "fulltext-search": {},
-    "vuepress-plugin-canonical": {
-      baseURL: "https://zksync.io", // base url for your canonical link, optional, default: ''
+    'fulltext-search': {},
+    'vuepress-plugin-canonical': {
+      baseURL: 'https://zksync.io', // base url for your canonical link, optional, default: ''
       stripExtension: false, // strip '.html' , optional, default: false
     },
     sitemap: {
-      hostname: "https://zksync.io",
+      hostname: 'https://zksync.io',
     },
   },
   themeConfig: {
-    logo: "/LogotypeLight.svg",
-    lastUpdated: "Last Updated",
-    repo: "matter-labs/zksync",
+    logo: '/LogotypeLight.svg',
+    lastUpdated: 'Last Updated',
+    repo: 'matter-labs/zksync',
     nav: [
       {
-        text: "User Docs",
-        link: "/userdocs/",
+        text: 'User Docs',
+        link: '/userdocs/',
       },
       {
-        text: "Developer Docs",
-        link: "/dev/",
+        text: 'Developer Docs',
+        link: '/dev/',
       },
       {
-        text: "API Reference",
-        link: "/api/",
+        text: 'API Reference',
+        link: '/api/',
       },
       {
-        text: "zkEVM",
-        link: "/zkevm/",
+        text: 'zkEVM',
+        link: '/zkevm/',
       },
       {
-        text: "Contact and Media",
-        link: "/contact.html",
+        text: 'Contact and Media',
+        link: '/contact.html',
       },
       {
-        text: "Uptime",
-        link: "/uptime",
+        text: 'Uptime',
+        link: '/uptime',
       },
     ],
     //displayAllHeaders: true,
     sidebar: {
-      "/legal/": ["/legal/terms", "/legal/privacy"],
-      "/userdocs/": [
+      '/legal/': ['/legal/terms', '/legal/privacy'],
+      '/userdocs/': [
         {
-          title: "Welcome to zkSync", // required
-          path: "/userdocs/", // optional, which should be a absolute path.
+          title: 'Welcome to zkSync', // required
+          path: '/userdocs/', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 0, // optional, defaults to 1
         },
         {
-          title: "Overview", // required
-          path: "/userdocs/intro.html", // optional, which should be a absolute path.
+          title: 'Overview', // required
+          path: '/userdocs/intro.html', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Technology", // required
-          path: "/userdocs/tech", // optional, which should be a absolute path.
+          title: 'Technology', // required
+          path: '/userdocs/tech', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Security", // required
-          path: "/userdocs/security", // optional, which should be a absolute path.
+          title: 'Security', // required
+          path: '/userdocs/security', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Learn By Watching", // required
-          path: "/userdocs/learnbywatching", // optional, which should be a absolute path.
+          title: 'Learn By Watching', // required
+          path: '/userdocs/learnbywatching', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Wallets", // required
-          path: "/userdocs/wallets", // optional, which should be a absolute path.
+          title: 'Wallets', // required
+          path: '/userdocs/wallets', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Tokens & Fees", // required
-          path: "/userdocs/tokens", // optional, which should be a absolute path.
+          title: 'Tokens & Fees', // required
+          path: '/userdocs/tokens', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Decentralization", // required
-          path: "/userdocs/decentralization", // optional, which should be a absolute path.
+          title: 'Decentralization', // required
+          path: '/userdocs/decentralization', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Tokenomics", // required
-          path: "/userdocs/tokenomics", // optional, which should be a absolute path.
+          title: 'Tokenomics', // required
+          path: '/userdocs/tokenomics', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Smart contracts", // required
-          path: "/userdocs/sc", // optional, which should be a absolute path.
+          title: 'Smart contracts', // required
+          path: '/userdocs/sc', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Privacy", // required
-          path: "/userdocs/privacy", // optional, which should be a absolute path.
+          title: 'Privacy', // required
+          path: '/userdocs/privacy', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Roadmap", // required
-          path: "/userdocs/roadmap", // optional, which should be a absolute path.
+          title: 'Roadmap', // required
+          path: '/userdocs/roadmap', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "FAQ", // required
-          path: "/userdocs/faq", // optional, which should be a absolute path.
+          title: 'FAQ', // required
+          path: '/userdocs/faq', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "Tutorials", // required
-          path: "/userdocs/tutorials", // optional, which should be a absolute path.
+          title: 'Tutorials', // required
+          path: '/userdocs/tutorials', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
       ],
-      "/dev": [
+      '/dev': [
         {
-          title: "Introduction", // required
-          path: "/dev/", // optional, which should be a absolute path.
+          title: 'Introduction', // required
+          path: '/dev/', // optional, which should be a absolute path.
           collapsable: false, // optional, defaults to true
         },
         {
-          title: "Payments", // required
-          path: "/dev/payments", // optional, which should be a absolute path.
+          title: 'Payments', // required
+          path: '/dev/payments', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
-          children: ["/dev/payments/basic", "/dev/payments/sending_transactions"],
+          children: ['/dev/payments/basic', '/dev/payments/sending_transactions'],
         },
         {
-          title: "Security", // required
-          path: "/dev/security", // optional, which should be a absolute path.
+          title: 'Security', // required
+          path: '/dev/security', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
-          children: ["/dev/security/approach", "/dev/security/bug-bounty", "/dev/security/disclosure", "/dev/security/bugs"],
+          children: ['/dev/security/approach', '/dev/security/bug-bounty', '/dev/security/disclosure', '/dev/security/bugs'],
         },
         {
-          title: "Smart contracts", // required
-          path: "/dev/contracts/", // optional, which should be a absolute path.
+          title: 'Smart contracts', // required
+          path: '/dev/contracts/', // optional, which should be a absolute path.
           collapsable: false, // optional, defaults to true
         },
-        "/dev/nfts",
-        "/dev/swaps",
-        "/dev/events",
+        '/dev/nfts',
+        '/dev/swaps',
+        '/dev/events',
       ],
-      "/api": [
+      '/api': [
         {
-          title: "Overview", // required
-          path: "/api/", // optional, which should be a absolute path.
+          title: 'Overview', // required
+          path: '/api/', // optional, which should be a absolute path.
           collapsable: false, // optional, defaults to true
           sidebarDepth: 0, // optional, defaults to 1
         },
-        "/api/changelog",
-        "/api/environments",
-        "/api/events",
+        '/api/changelog',
+        '/api/environments',
+        '/api/events',
         {
-          title: "API v0.1", // required
-          path: "/api/v0.1", // optional, which should be a absolute path.
+          title: 'API v0.1', // required
+          path: '/api/v0.1', // optional, which should be a absolute path.
           collapsable: false, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "API v0.2", // required
-          path: "/api/v0.2", // optional, which should be a absolute path.
+          title: 'API v0.2', // required
+          path: '/api/v0.2', // optional, which should be a absolute path.
           collapsable: false, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
         },
         {
-          title: "JavaScript SDK", // required
-          path: "/api/sdk/js", // optional, which should be a absolute path.
+          title: 'JavaScript SDK', // required
+          path: '/api/sdk/js', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 2, // optional, defaults to 1
           children: [
-            "/api/sdk/js/tutorial",
-            "/api/sdk/js/providers",
-            "/api/sdk/js/accounts",
-            "/api/sdk/js/utils",
-            "/api/sdk/js/nfts",
-            "/api/sdk/js/types",
-            "/api/sdk/js/browser-bundled",
+            '/api/sdk/js/tutorial',
+            '/api/sdk/js/providers',
+            '/api/sdk/js/accounts',
+            '/api/sdk/js/utils',
+            '/api/sdk/js/nfts',
+            '/api/sdk/js/types',
+            '/api/sdk/js/browser-bundled',
           ],
         },
         {
-          title: "Rust SDK", // required
-          path: "/api/sdk/rust", // optional, which should be a absolute path.
+          title: 'Rust SDK', // required
+          path: '/api/sdk/rust', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 2, // optional, defaults to 1
-          children: ["/api/sdk/rust/tutorial"],
+          children: ['/api/sdk/rust/tutorial'],
         },
         {
-          title: "Java SDK", // required
-          path: "/api/sdk/java", // optional, which should be a absolute path.
+          title: 'Java SDK', // required
+          path: '/api/sdk/java', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 2, // optional, defaults to 1
-          children: ["/api/sdk/java/tutorial", "/api/sdk/java/interfaces"],
+          children: ['/api/sdk/java/tutorial', '/api/sdk/java/interfaces'],
         },
         {
-          title: "Swift SDK", // required
-          path: "/api/sdk/swift", // optional, which should be a absolute path.
+          title: 'Swift SDK', // required
+          path: '/api/sdk/swift', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 2, // optional, defaults to 1
-          children: ["/api/sdk/swift/tutorial"],
+          children: ['/api/sdk/swift/tutorial'],
         },
         {
-          title: "Python SDK", // required
-          path: "/api/sdk/python", // optional, which should be a absolute path.
+          title: 'Python SDK', // required
+          path: '/api/sdk/python', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 2, // optional, defaults to 1
-          children: ["/api/sdk/python/tutorial"],
+          children: ['/api/sdk/python/tutorial'],
         },
-        "/api/sdk/dart",
+        '/api/sdk/dart',
         {
-          title: "zkCheckout SDK", // required
-          path: "/api/sdk/checkout", // optional, which should be a absolute path.
+          title: 'zkCheckout SDK', // required
+          path: '/api/sdk/checkout', // optional, which should be a absolute path.
           collapsable: true, // optional, defaults to true
           sidebarDepth: 2, // optional, defaults to 1
           children: [
-            "/api/sdk/checkout/tutorial",
-            "/api/sdk/checkout/checkout-manager",
-            "/api/sdk/checkout/types",
-            "/api/sdk/checkout/constants",
-            "/api/sdk/checkout/protocol",
-            "/api/sdk/checkout/appendix-a",
+            '/api/sdk/checkout/tutorial',
+            '/api/sdk/checkout/checkout-manager',
+            '/api/sdk/checkout/types',
+            '/api/sdk/checkout/constants',
+            '/api/sdk/checkout/protocol',
+            '/api/sdk/checkout/appendix-a',
           ],
         },
-        "/api/sdk/crypto",
+        '/api/sdk/crypto',
       ],
     },
   },
   head: [
-    ["script", { src: "/__/firebase/7.13.2/firebase-app.js", defer: true }, ""],
-    ["script", { src: "/__/firebase/7.13.2/firebase-analytics.js", defer: true }, ""],
-    ["script", { src: "/__/firebase/init.js", defer: true }, ""],
+    ['script', { src: '/__/firebase/7.13.2/firebase-app.js', defer: true }, ''],
+    ['script', { src: '/__/firebase/7.13.2/firebase-analytics.js', defer: true }, ''],
+    ['script', { src: '/__/firebase/init.js', defer: true }, ''],
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '57x57', type: 'image/x-icon', href: '/apple-icon-57x57.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '60x60', type: 'image/x-icon', href: '/apple-icon-60x60.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '72x72', type: 'image/x-icon', href: '/apple-icon-72x72.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '76x76', type: 'image/x-icon', href: '/apple-icon-76x76.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '114x114', type: 'image/x-icon', href: '/apple-icon-114x114.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '120x120', type: 'image/x-icon', href: '/apple-icon-120x120.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '144x144', type: 'image/x-icon', href: '/apple-icon-144x144.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '152x152', type: 'image/x-icon', href: '/apple-icon-152x152.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', type: 'image/x-icon', href: '/apple-icon-180x180.png' }],
+    ['link', { rel: 'icon', sizes: '192x192', type: 'image/png', href: '/android-icon-192x192.png' }],
+    ['link', { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/android-icon-36x36.png' }],
+    ['link', { rel: 'icon', sizes: '96x96', type: 'image/png', href: '/android-icon-96x96.png' }],
+    ['link', { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/android-icon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
     //Hack: redirects the user to the zksync.io index page when clicking on the logo
     [
-      "script",
+      'script',
       {},
       `
       const logoUrlChanger = setInterval(function() {
@@ -279,7 +296,7 @@ homeEl.setAttribute("href", "https://zksync.io"); homeEl.setAttribute("onclick",
     ],
     //Hack: scroll to anchor
     [
-      "script",
+      'script',
       {},
       `
       window.onload = function() {
@@ -296,4 +313,5 @@ homeEl.setAttribute("href", "https://zksync.io"); homeEl.setAttribute("onclick",
       `,
     ],
 
-], };
+  ],
+};
