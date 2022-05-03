163dbaa5207f3b31dfb30d4df2a66dada1b45614
bxpana <42230936+bxpana@users.noreply.github.com>
2022-05-02 17:07:38 -0700

    Update config.ts
    Try to fix anchor text

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -146,7 +146,7 @@ export = defineConfig(_ctx => {
           },
           {
             title: 'FAQ', // required
-            path: '/userdocs/faq', // optional, which should be a absolute path.
+            path: '/userdocs/faq/', // optional, which should be a absolute path.
             collapsable: true, // optional, defaults to true
             sidebarDepth: 1, // optional, defaults to 1
           },
