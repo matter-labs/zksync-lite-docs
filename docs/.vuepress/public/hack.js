222c941064cfd5f80c5cc964b8e3ddbfc72b5c0c
Jack Hamer <jackhamer0990@gmail.com>
2022-05-09 17:26:12 +0300

    fix: test commit, check deploy

--- docs/.vuepress/public/hack.js
+++ docs/.vuepress/public/hack.js
@@ -6,17 +6,25 @@ function locationHashChanged (event) {
       return;
     }
     const currentLocation = (event.path && event.path.length > 0 && event.path[0] && event.path[0].location) ? event.path[0].location : window.location;
+    console.log("\n\ncurrentLocation: ", currentLocation)
+    console.log("currentLocation.hash: ", currentLocation.hash)
+    console.log("lastHash: ", lastHash, currentLocation && currentLocation.hash !== "" && currentLocation.hash !== lastHash)
     if (currentLocation && currentLocation.hash !== "" && currentLocation.hash !== lastHash) {
       const element = document.querySelector(currentLocation.hash);
+      console.log("element: ", element)
       if (element) {
         if (event.hasOwnProperty('stopPropagation')) {
+          console.log(1);
           event.stopPropagation();
         }
         if (element.hasOwnProperty('scrollIntoViewIfNeeded')) {
+          console.log(2);
           element.scrollIntoViewIfNeeded(true);
         } else {
+          console.log(3);
           element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
         }
+        console.log("Set lastHash to ", currentLocation.hash);
         lastHash = currentLocation.hash;
         return false;
       }
@@ -27,20 +35,24 @@ function locationHashChanged (event) {
 }
 
 window.addEventListener('loadeddata', (event) => {
+  console.log("loadeddata");
   locationHashChanged(event);
 });
 
 document.addEventListener('readystatechange', (event) => {
+  console.log("readystatechange");
   locationHashChanged(event);
 });
 
 // Script #1: Processing scroll-to-hash
 window.addEventListener('hashchange', (event) => {
+  console.log("hashchange");
   locationHashChanged(event);
 });
 
 // Script #2: Changing logo & home-link behaviour
 window.addEventListener('DOMContentLoaded', (event) => {
+  console.log("DOMContentLoaded");
   locationHashChanged(event);
   const singleElement = document.querySelector('a.home-link');
   const newHost = 'https://zksync.io';
